7f59a8cbec1c6b2f7050a1f9c9b2fe117c3fae4e
Quique1012 <enrique_gg@hotmail.es>
2023-11-21 12:30:57 +0100

    AGREGO CAMBIOS

--- app/src/main/java/com/example/preguntapp/MainActivity.java
+++ app/src/main/java/com/example/preguntapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.preguntapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // add listener to play card
+        CardView play = findViewById(R.id.cardView);
+        play.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            Intent playIntent = new Intent(MainActivity.this,Jugar.class);
+
+            startActivity(playIntent);
+
+            }
+
+        });
+        // add listener to play card
+        CardView ayuda = findViewById(R.id.cardViewAyuda);
+        play.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Intent playIntent = new Intent(MainActivity.this,Ayuda.class);
+
+                startActivity(playIntent);
+
+            }
+
+        });
     }
 }
